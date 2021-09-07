9f5ee6a49cc580799d2f8efff5de9fc5646cbe8a
Bastien Petit <bastien.petit77@gmail.com>
2021-09-07 12:41:52 +0200

    lunch breack 2

--- src/Controller/Article/CreateArticleController.php
+++ src/Controller/Article/CreateArticleController.php
@@ -7,6 +7,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CreateArticleController extends AbstractController
 {
@@ -15,11 +16,19 @@ class CreateArticleController extends AbstractController
      * @Route("/article/creer", name="creer_article")
      */
 
-    public function create()
+    public function create(Request $request)
     {
         $article = new Article(); 
 
         $form = $this->createForm(ArticleType::class, $article);  
+
+        $form->handleRequest($request); 
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            dd($request);
+            
+        }
 
         return $this->render('article/create.html.twig', [
             "formArticle" => $form->createView()
